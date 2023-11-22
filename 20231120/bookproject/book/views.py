abf6ad57ace5f711db9b1c79a96cb3a52edf6815
djangouser <m-nunomura@stroog.com>
2023-11-22 14:44:53 +0900

    5-7end

--- 20231120/bookproject/book/views.py
+++ 20231120/bookproject/book/views.py
@@ -1,7 +1,10 @@
+from typing import Any
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.views import generic
 from . import models
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy,reverse
 
 # Create your views here.
 class ListBookView(generic.ListView):
@@ -32,3 +35,20 @@ class UpdateBookView(generic.UpdateView):
 def index(request):
     object_list = models.Book.objects.order_by("category")
     return render(request,"book/index.html",{"object_list":object_list})
+
+class CreateReviewView(generic.CreateView):
+    template_name = "book/review_form.html"
+    model = models.Review
+    fields = ("book","title","text","rate",)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["book"] = models.Book.objects.get(pk=self.kwargs["book_id"])
+        return context
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse("book:detail-book",kwargs={"pk":self.object.book.id})
