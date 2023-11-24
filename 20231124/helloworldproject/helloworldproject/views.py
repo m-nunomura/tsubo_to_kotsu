fbb6542afae4966b6fd2aecd9660a7c2ffb1507a
djangouser <m-nunomura@stroog.com>
2023-11-24 13:35:58 +0900

    20231124_3-10end

--- 20231124/helloworldproject/helloworldproject/views.py
+++ 20231124/helloworldproject/helloworldproject/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.views import generic
 
 def helloworldfunc(request):
     responseobject = HttpResponse("<h1>helloworld</h1>")
     return responseobject
+
+class HelloWorldClass(generic.TemplateView):
+    template_name = "hello.html"
