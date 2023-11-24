f393224d0208484a74db40c3e1c305b9f3c70631
djangouser <m-nunomura@stroog.com>
2023-11-24 15:05:56 +0900

    20231124_4-10end

--- 20231124/bookproject/book/views.py
+++ 20231124/bookproject/book/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from . import models
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -11,3 +12,14 @@ class ListBookView(generic.ListView):
 class DetailBookView(generic.DetailView):
     template_name = "book/book_detail.html"
     model = models.Book
+
+class CreateBookView(generic.CreateView):
+    template_name = "book/book_create.html"
+    model = models.Book
+    fields = ("title","text","category",)
+    success_url = reverse_lazy("book:list-book")
+
+class DeleteBookView(generic.DeleteView):
+    template_name = "book/book_delete.html"
+    model = models.Book
+    success_url = reverse_lazy("book:list-book")
