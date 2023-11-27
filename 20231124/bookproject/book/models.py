7b086d898aedce9188028c1eb3ec5cd277af906d
djangouser <m-nunomura@stroog.com>
2023-11-27 11:09:32 +0900

    20231127_5-7end

--- 20231124/bookproject/book/models.py
+++ 20231124/bookproject/book/models.py
@@ -1,13 +1,25 @@
 from django.db import models
+from . import consts
 
 # Create your models here.
 
 CATEGORY = (("business","ビジネス"),("life","生活"),("other","その他"),)
+RATE_CHOICE = [(x,str(x))for x in range(0,consts.MAX_RATE + 1)]
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
     category = models.CharField(max_length=100,choices=CATEGORY)
+
+    def __str__(self):
+        return self.title
+    
+class Review(models.Model):
+    book = models.ForeignKey(Book,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    rate = models.IntegerField(choices=RATE_CHOICE)
+    user = models.ForeignKey("auth.User",on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
