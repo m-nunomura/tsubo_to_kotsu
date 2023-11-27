d2b879fbedc1880ebff87a90806f7bec17e928d2
djangouser <m-nunomura@stroog.com>
2023-11-27 13:10:08 +0900

    20231127_5-10end

--- 20231124/bookproject/book/views.py
+++ 20231124/bookproject/book/views.py
@@ -5,29 +5,30 @@ from django.shortcuts import render,redirect
 from django.views import generic
 from . import models
 from django.urls import reverse_lazy,reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
-class ListBookView(generic.ListView):
+class ListBookView(LoginRequiredMixin,generic.ListView):
     template_name = "book/book_list.html"
     model = models.Book
 
-class DetailBookView(generic.DetailView):
+class DetailBookView(LoginRequiredMixin,generic.DetailView):
     template_name = "book/book_detail.html"
     model = models.Book
 
-class CreateBookView(generic.CreateView):
+class CreateBookView(LoginRequiredMixin,generic.CreateView):
     template_name = "book/book_create.html"
     model = models.Book
     fields = ("title","text","category","thumbnail",)
     success_url = reverse_lazy("book:list-book")
 
-class DeleteBookView(generic.DeleteView):
+class DeleteBookView(LoginRequiredMixin,generic.DeleteView):
     template_name = "book/book_delete.html"
     model = models.Book
     success_url = reverse_lazy("book:list-book")
 
-class UpdateBookView(generic.UpdateView):
+class UpdateBookView(LoginRequiredMixin,generic.UpdateView):
     template_name = "book/book_update.html"
     model = models.Book
     fields = ("title","text","category","thumbnail",)
@@ -37,7 +38,7 @@ def index_view(request):
     object_list = models.Book.objects.order_by("category")
     return render(request,"book/index.html",{"object_list":object_list})
 
-class CreateReviewView(generic.CreateView):
+class CreateReviewView(LoginRequiredMixin,generic.CreateView):
     template_name = "book/review_form.html"
     model = models.Review
     fields = ("book","title","text","rate",)
