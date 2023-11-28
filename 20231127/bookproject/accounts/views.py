7ec31e462473960277fa5ee2d453c703733dd3af
djangouser <m-nunomura@stroog.com>
2023-11-28 15:29:57 +0900

    20231128_5-6end

--- 20231127/bookproject/accounts/views.py
+++ 20231127/bookproject/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+
+from . import forms
 
 # Create your views here.
+class SignupView(generic.CreateView):
+    template_name = "accounts/signup.html"
+    model = User
+    form_class = forms.SignupForm
+    success_url = reverse_lazy("book:index")
