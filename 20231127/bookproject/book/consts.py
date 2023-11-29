c665693eb8eb9d112d2c5b79e5f3f3061f39c143
djangouser <m-nunomura@stroog.com>
2023-11-29 10:30:02 +0900

    20231129_5-14end

--- 20231127/bookproject/book/consts.py
+++ 20231127/bookproject/book/consts.py
@@ -1,3 +1,4 @@
 CATEGORY = (("bussiness","ビジネス"),("life","生活"),("other","その他"),)
 MAX_RATE = 5
 RATE_CHOISES = [(x,str(x))for x in range(0,MAX_RATE +1)]
+ITEM_PER_PAGE = 2
--- 20231127/bookproject/book/views.py
+++ 20231127/bookproject/book/views.py
@@ -8,7 +8,8 @@ from django.urls import reverse_lazy,reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
 from django.db.models import Avg
-from . import models
+from django.core.paginator import Paginator
+from . import models,consts
 
 # Create your views here.
 
@@ -16,6 +17,7 @@ from . import models
 class ListBookView(LoginRequiredMixin,generic.ListView):
     template_name = "book/book_list.html"
     model = models.Book
+    paginate_by = consts.ITEM_PER_PAGE
 
 class DetailBookView(LoginRequiredMixin,generic.DetailView):
     template_name = "book/book_detail.html"
@@ -64,7 +66,10 @@ class UpdateBookView(LoginRequiredMixin,generic.UpdateView):
 def index(request):
     object_list = models.Book.objects.order_by("-id")
     ranking_list = models.Book.objects.annotate(avg_rating=Avg("review__rate")).order_by("-avg_rating")
-    return render(request,"book/book_index.html",{"object_list":object_list,"ranking_list":ranking_list})
+    paginator = Paginator(ranking_list,consts.ITEM_PER_PAGE)
+    page_number = request.GET.get("page",1)
+    page_obj = paginator.page(page_number)
+    return render(request,"book/book_index.html",{"object_list":object_list,"ranking_list":ranking_list,"page_obj":page_obj})
 
 class CreateReviewView(LoginRequiredMixin,generic.CreateView):
     template_name = "book/book_review.html"
